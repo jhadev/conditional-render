37fc466b1792e7ccb402965f7fe7528accf90827
jhadev <joshappeldev@gmail.com>
2019-08-23 08:28:55 -0400

    better example

--- src/components/Year.js
+++ src/components/Year.js
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const Year = props => {
-  console.log(props);
-  const [year, setYear] = useState(0);
-
-  // this effect depends on the props passed down from above. It will run everytime they change.
-  // when the button is pressed they year in the state of this component will be updated to reflect the changes and display that on the page.
-  useEffect(() => {
-    setYear(props.yearBorn + props.age);
-  }, [props.age, props.yearBorn]);
-
-  return <h1>The Year is: {year}</h1>;
-};
+const Year = ({ age, yearBorn }) => (
+  // destructure props in the parameter
+  <h1>The Year is: {age + yearBorn}</h1>
+);
 
 export default Year;
